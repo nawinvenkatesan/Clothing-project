44f3c4515f32028c6e7a486d0810b867cf5862f1
nawinvenkatesan <nawinvenkatesan@gmail.com>
2024-05-27 13:08:53 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+var sidenav = document.querySelector(".side-navbar")
+sidenav.style.display = "none"
+
+function showNavbar() {
+    sidenav.style.display = "block"
+
+}
+
+function closeNavbar() {
+    sidenav.style.display = "none"
+
+}
